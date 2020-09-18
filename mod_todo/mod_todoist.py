315ba179fc285b26dfa251d58aabee79f3afddff
morningshane <70739280+morningshane@users.noreply.github.com>
2020-09-18 22:24:31 +1000

    Update mod_todoist.py

--- mod_todo/mod_todoist.py
+++ mod_todo/mod_todoist.py
@@ -5,11 +5,11 @@ import options
 class ToDo:
     def __init__(self, opts):
         logging.debug("Todo API: TODOIST")
-        self.api = TodoistAPI(opts['4b21447369cd11bdfb05ee6ee73987967e37a9e2'])
+        self.api = todoist.TodoistAPI(opts['4b21447369cd11bdfb05ee6ee73987967e37a9e2'])
         self.api.sync()
 
     def list(self):
-        items = []
+        items = ['infowindow']
         # Loop through original array from Todoist and pull out 
         # items of interest
         for item in self.api.state['items']:
