c63e46445d00f7d874d11a4d7e9ee88614b6a59d
morningshane <70739280+morningshane@users.noreply.github.com>
2020-09-18 16:31:52 +1000

    Initial commit

---
+++ mod_todo/mod_todoist.py
@@ -0,0 +1,31 @@
+import todoist
+import logging
+from todoist import models
+from todoist import api
+import options
+
+class ToDo:
+    def __init__(self):
+        logging.debug("Todo API: TODOIST")
+        self.api = todoist.TodoistAPI('1e5212f77e2020066a7837c5e6ca1785674a4177')
+        self.api.sync()
+
+    def list(self):
+        items = []
+        # Loop through original array from Todoist and pull out 
+        # items of interest
+        for item in self.api.state['items']:
+            if item['checked'] == 0:
+                items.append({
+                    "content": item['content'],
+                    "priority": item['priority'],
+                })
+
+        # Sort the array by priority    
+        items = sorted(items, key = lambda i: i['priority'])
+
+        # Reverse list, since Todoist sets priority in reverse.
+        # On web interface HIGH=Priority1, but stored in API as 4. who knows?!
+        items.reverse()
+
+        return items
