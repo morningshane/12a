2604300c54feec0661222fa6a36b5cea7d07648d
morningshane <70739280+morningshane@users.noreply.github.com>
2020-09-20 21:52:09 +1000

    Update infowindow.py

--- infowindow.py
+++ infowindow.py
@@ -146,7 +146,7 @@ def main():
     deg_symbol = u"\u00b0"
     iw.bitmap(2, 2, weather['icon'])
     iw.text(70, 2, weather['description'].title(), 'robotoBlack24', 'black')
-    iw.text(70, 35, name, 'robotoRegular18', 'black')
+    iw.text(70, 35, name['name'], 'robotoRegular18', 'black')
     
     # Temp ( adjust for str length )
     (t_x, t_y) = iw.getFont('robotoBlack48').getsize(str(weather['temp_cur'])+deg_symbol)
