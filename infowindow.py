48d7f3879df1fab1a48aaa7a0f28c2ab69b91438
morningshane <70739280+morningshane@users.noreply.github.com>
2020-09-24 19:41:55 +1000

    Update infowindow.py

--- infowindow.py
+++ infowindow.py
@@ -114,10 +114,10 @@ def main():
     logging.debug("-----------------------------------------------------------------------")
     t_y = 94
     for todo_item in todo_items:
-        iw.text(333, t_y, str(todo_item['content']), 'robotoRegular18', 'black')
+        iw.text(333, t_y, str(todo_item[utf8_encode'content']), 'robotoRegular18', 'black')
         t_y = (t_y + 24)
         iw.line(325, (t_y - 2), 640, (t_y - 2), 'black')
-        logging.debug("ITEM: "+todo_item['content'])
+        logging.debug("ITEM: "+todo_item[utf8_encode'content'])
 
     # DISPLAY CALENDAR INFO
     # =========================================================================
@@ -133,11 +133,11 @@ def main():
         (x, y) = iw.text(3, c_y, str(cal_item['date']), 'robotoRegular14', 'black')
         iw.line((dt_x + 5), c_y, (dt_x + 5), (c_y +32), 'black')
         iw.text(3, (c_y + 15), str(cal_item['time']), 'robotoRegular14', 'black')
-        iw.text((dt_x + 7), (c_y + 5), iw.truncate(str(cal_item['content']), 'robotoRegular18'), 'robotoRegular18', 'black')
+        iw.text((dt_x + 7), (c_y + 5), iw.truncate(str(cal_item[utf8_encode'content']), 'robotoRegular18'), 'robotoRegular18', 'black')
         c_y = (c_y + 32)
         iw.line(0, (c_y - 2), 313, (c_y - 2), 'black')
-        # logging.debug(u"ITEM: "+str(cal_item['date']), str(cal_item['time']), str(cal_item[u'content']))
-        logging.debug(u"ITEM: "+str(cal_item['content']))
+        # logging.debug(u"ITEM: "+str(cal_item['date']), str(cal_item['time']), str(cal_item[utf8_encode'content']))
+        logging.debug(u"ITEM: "+str(cal_item[utf8_encode'content']))
 
     # DISPLAY WEATHER INFO
     # =========================================================================
