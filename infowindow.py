778bd149987bdf88547632d39bc1f946ff09e295
morningshane <70739280+morningshane@users.noreply.github.com>
2020-09-30 16:06:02 +1000

    Update infowindow.py

--- infowindow.py
+++ infowindow.py
@@ -13,7 +13,6 @@ libdir = os.path.join(os.path.dirname(os.path.dirname(os.path.realpath(__file__)
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
-reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # Select pluggable module for todo list, calendar and weather.
